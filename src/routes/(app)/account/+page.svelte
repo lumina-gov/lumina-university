1c00a5e5ec295d4377fbc8ae3807a7223533dad6
Romeo-V1 <romeo.nguyen01@yahoo.com>
2023-03-28 18:31:11 +1030

    Added account / billing page

--- src/routes/(app)/account/+page.svelte
+++ src/routes/(app)/account/+page.svelte
@@ -1,21 +1,91 @@
 <script lang="ts">
 import Button from "$lib/controls/Button.svelte"
 import type { PageData } from "./$types"
-
+import Lumina from "$lib/icons/Lumina.svelte"
+import Grid from "$lib/layouts/Grid.svelte"
+import GridItem from "$lib/layouts/GridItem.svelte"
+import VerticalLayout from "$lib/layouts/VerticalLayout.svelte"
+import Heading from "$lib/display/Heading.svelte"
+import Paragraph from "$lib/display/Paragraph.svelte"
+import AccountCircle from "svelte-material-icons/AccountCircle.svelte"
+import CreditCard from "svelte-material-icons/CreditCard.svelte"
+import RocketLaunch from "svelte-material-icons/RocketLaunch.svelte"
 export let data: PageData
-
 </script>
+<hr>
+<Grid>
+    <GridItem
+        columns={{
+            mobile: "span 4",
+            tablet: "span 8",
+            laptop: "span 16",
+        }}
+    >
+        <VerticalLayout
+            max_width={650}
+            padding={16}
+            vertical_padding={40}>
+            <Heading left_icon={Lumina}>My Account</Heading>
+            <Paragraph>
+                Your Light University account is managed through the Lumina Government Website
+            </Paragraph>
+            <div>
+                <Button
+                    style="translucent"
+                    href={data.customer_portal_url}
+                    hug={true}
+                    left_icon={AccountCircle}>
+                    Manage Account
+                </Button>
+            </div>
+        </VerticalLayout>
+    </GridItem>
+</Grid>
+<hr>
+<Grid>
+    <GridItem
+        columns={{
+            mobile: "span 4",
+            tablet: "span 8",
+            laptop: "span 16",
+        }}
+    >
+        <VerticalLayout
+            horizontal_padding={16}
+            max_width={650}
+            vertical_padding={40}>
+            <Heading
+                left_icon={CreditCard}
+                level={3}>Billing</Heading>
+            <Paragraph>
+                Manage your education subscription, payment details and invoices through our payment provider.
+            </Paragraph>
+            <div class="button-wrapper">
+                <Button
+                    style="translucent"
+                    href={data.customer_portal_url}
+                    left_icon={AccountCircle}>
+                    Manage Account
+                </Button>
+                <Button
+                    style="branded"
+                    href={data.customer_portal_url}
+                    right_icon={RocketLaunch}>
+                    Start Subscription
+                </Button>
+            </div>
+        </VerticalLayout>
+    </GridItem>
 
-<h2>Billing</h2>
-<p>Manage payment methods through stripe</p>
-<Button
-    style="branded"
-    href={data.customer_portal_url}>
-    Manage Billing
-</Button>
 
-<Button
-    style="branded"
-    href={data.checkout_session_url}>
-    Start Subscription
-</Button>
+</Grid>
+
+
+<style lang="stylus">
+@import "variables"
+
+.button-wrapper
+    display flex
+    gap 16px
+
+</style>
