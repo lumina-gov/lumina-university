15bb6c098cf3829f4eb5ef40de848f1e194937e2
Albert <albert@framework.tools>
2023-05-23 21:09:15 +0930

    Improve session handling code for invalid tokens

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,6 @@
 import { alerts_init } from "$lib/stores/alerts"
 import { init_urql } from "$lib/stores/graphql"
-import { get_me, InvalidTokenError } from "$lib/api/user"
+import { get_me } from "$lib/api/user"
 import { browser } from "$app/environment"
 import { set_cookie } from "$lib/utils/cookie"
 import { user_store_init } from "$lib/stores/user_store"
@@ -13,10 +13,9 @@ export async function load ({ data: { auth_token, login_url, lumina_domain }}) {
     try {
         user_store.user = await get_me(graph, alerts)
     } catch (e) {
-        if (browser && e instanceof InvalidTokenError) {
+        if (browser && e && typeof e === "object" && "code" in e && e.code === "INVALID_TOKEN") {
             set_cookie("token", null)
         }
-        user_store.user = null
         user_store.auth_token = null
     }
 
